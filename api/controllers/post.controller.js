70c5781ad8507b6bc90a123aea729d44ef33606a
kunals001 <kunaltrad555@gmail.com>
2025-06-11 17:47:06 +0530

    update post

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -23,3 +23,24 @@ export const createPost = async(req, res) => {
         res.status(500).json({success: false, message: error.message });
     }
 }
+
+export const updatePost = async(req,res) =>{
+    try {
+
+        const user = await User.findById(req.user.id).select("-password");
+
+        if(!user.isAdmin){
+            return res.status(400).json({success: false, message: "User not found" });
+        }
+
+        const postId = req.params.postId;
+        const updateData = req.body;
+
+        const updatedPost = await Post.findByIdAndUpdate(postId,updateData,{new:true});
+        res.status(200).json({success: true, message: "Post updated successfully",updatedPost });
+
+    } catch (error) {
+        console.log("Error in updatePost ", error);
+        return res.status(500).json({ success: false, message: "Server error in updatePost" });
+    }
+}
--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -2,8 +2,9 @@ import express from "express";
 const router = express.Router();
 
 import {protectRoute} from "../utils/protectRoute.js"
-import {createPost} from "../controllers/post.controller.js"
+import {createPost,updatePost} from "../controllers/post.controller.js"
 
 router.post("/create-post",protectRoute,createPost)
+router.put("/update-post/:postId",protectRoute,updatePost)
 
 export default router
